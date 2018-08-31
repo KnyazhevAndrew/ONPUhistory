19ebbba6afefad00b1aff25d5b9d04edc9f83a54
Andrey Knyazhev <knyazz68@gmail.com>
2018-08-31 08:52:55 +0300

    1918 creation added

--- historyOpu/js/init.js
+++ historyOpu/js/init.js
@@ -1,12 +1,7 @@
-/*
-	Telephasic 1.0 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 skel.init({
 	/*
-		Docs @ http://skeljs.org/docs
+	
 	*/
 	prefix: 'css/style',
 	resetCSS: true,
